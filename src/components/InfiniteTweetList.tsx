523bfd6f64160596468a9b21304151a2399a4586
Kimmi17 <dangthikimthuybl@gmail.com>
2023-09-21 22:30:57 +0300

    Following Tabs

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -5,6 +5,7 @@ import { useSession } from "next-auth/react";
 import { VscHeart, VscHeartFilled } from "react-icons/vsc";
 import { IconHoverEffect } from "./IconHoverEffect";
 import { api } from "~/utils/api";
+import { LoadingSppiner } from "./LoadingSpinner";
 
 type Tweet = {
   id: string;
@@ -30,7 +31,7 @@ export function InfiniteTweetList({
   fetchNewTweets,
   hasMore = false,
 }: InfiniteTweetListProps) {
-  if (isLoading) return <h1>Loading...</h1>;
+  if (isLoading) return <LoadingSppiner />;
   if (isError) return <h1>Error...</h1>;
   if (tweets == null) return null;
 
@@ -46,7 +47,7 @@ export function InfiniteTweetList({
         dataLength={tweets.length}
         next={fetchNewTweets}
         hasMore={hasMore}
-        loader={"Loading..."}
+        loader={<LoadingSppiner />}
       >
         {tweets.map((tweet) => {
           return <TweetCard key={tweet.id} {...tweet} />;
